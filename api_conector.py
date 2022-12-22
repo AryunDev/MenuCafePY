c51b9105456ef8ec9f43f5016fec55d1a7ffd837
AryunDev <josematamoros916@gmail.com>
2022-12-22 19:44:30 -0300

    comit pa que el bastian meta mano

--- api_conector.py
+++ api_conector.py
@@ -1,29 +1,32 @@
 import requests
-from flask import Flask
-import pprint
-import json
+# from flask import Flask
+# import pprint 
+# import json
 
 class Api():
 
-    def __init__(self):     
+    def __init__(self,url):
+        self.url = url   
       
-
+    def mostrar_producto():
         url = "http://localhost:5000/productos"
         response = requests.get(url)
 
-        print(response)
+        # print(response)
+
         print(response.text)
+        # esto sirve para hacer validaciones segun el julio
         print(response.status_code)
 
 
-    def __init__(self):     
-        pepinho = {'Content-Type': 'application/json'}
-        pepito = {"id": 10, "nombre": "Pié De Limón", "descripcion": "Masa, Azucar, Limó", "valor": 2500}         
+    def crear_producto(self, data):    
+        headers = {'Content-Type': 'application/json'}
+        data = '{"id": 10, "nombre": "Pié De Limón", "descripcion": "Masa, Azucar, Limó", "valor": 2500}'    
         
         url = "http://localhost:5000/productos"
-        response = requests.post(url, data=pepito, headers=pepinho)
-
-        print(response)
+        response = requests.post(self, url, data=data, headers=headers)
+        
+        # print(response)
         print(response.text)
         print(response.status_code)
         
