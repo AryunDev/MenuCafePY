b699a6e9ad056cac7c73da63cff27f22075f370a
AryunDev <josematamoros916@gmail.com>
2022-12-24 15:11:43 -0300

    ultimos cambios al menu

--- api_conector.py
+++ api_conector.py
@@ -1,6 +1,5 @@
 import requests
-from flask import Flask
-import pprint 
+# from flask import Flask
 import json
 
 
@@ -11,8 +10,9 @@ class Api():
     def __init__(self, url):
         self.url = url
         
-      
-    def mostrar_productos(self):
+    
+    def mostrar_productos():
+
         url = "http://localhost:5000/productos"
         response = requests.get(url)
 
@@ -22,38 +22,47 @@ class Api():
         # esto sirve para hacer validaciones segun el julio
         print(response.status_code)
 
-    def mostrar_producto(self, id):
+
+
+    def mostrar_producto(id):
+
         url = f"http://localhost:5000/productos/{id}"
         response = requests.get(url)
-        print(response.text)
+        response = response.json()        
 
-
-
-    def crear_producto(self):    
-        
-        # headers = {'Content-Type': 'application/json'}
-        data = {"id": 10, "nombre": "Pié De Limón", "descripcion": "Masa, Azucar, Limó", "valor": 2500}  
-        
-        # url = "http://localhost:5000/productos"
-        response = requests.post(self, url, data=data)
-        
-        print(response)
-        print(response.text)
-        print(response.status_code)
-
-    def actualizar_producto(self, id):
-        url= f"http://localhost:5000/productos/{id}" 
-        data = {"id": 1,"nombre": "Donuts","descripcion": "rellenas de chocolate","valor": 2500}
-
-        response = requests.put(url, data=data)
-        # print(response.text)
         try:
-            response.status_code == 200
+            print(response)
+        except:
+            print('Error al intentar mostrar los datos')
+
+
+
+    # def crear_producto(self, data):    
+        
+    #     headers = {'Content-Type': 'application/json'}
+    #     #data = f'{"id": {id}, "nombre": {nombre}, "descripcion": {descripcion}, "valor": {valor}}'
+
+        
+    #     # url = "http://localhost:5000/productos"
+    #     response = requests.post(self, url, data=data, headers = headers)
+        
+    #     print(response)
+    #     print(response.text)
+    #     print(response.status_code)
+
+    def actualizar_producto(id, data):
+        url= f"http://localhost:5000/productos/{id}" 
+        # data = {"id": 1,"nombre": "Donuts","descripcion": "rellenas de chocolate","valor": 2500}    
+        try:
+            response = requests.put(url, data=data)
+            response = response.json()
+            print(response)
+            # response.status_code == 200
             print('Datos actualizados correctamente')
         except:
             print('Error al actualizar los datos')
 
-    def eliminar_producto(self, id):
+    def eliminar_producto(id):
 
         url= f"http://localhost:5000/productos/{id}" 
         response = requests.delete(url)
@@ -75,9 +84,9 @@ obj = Api(url)
 # obj.mostrar_productos()
 
 
+# obj.mostrar_producto()
+# obj.crear_producto()
 # obj.mostrar_productos()
-obj.crear_producto()
-obj.mostrar_productos()
 
     
 
--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from api_conector import Api
+
+
 import os
 def limpiarpantalla():
     os.system('cls')
@@ -18,22 +21,41 @@ class Menu:
         ''')
 
         op = input("Ingrese una opcion:\n").capitalize()
+        print('\n')
         limpiarpantalla()
 
         if op == 'A':
-            id = int(input("Asigne un id a su nuevo producto:\n"))
-            nombre = input("Ingrese el nombre del producto:\n")
-            descripcion = input("Ingrese una descripcion para su producto:\n")
-            precio = int(input("Ingrese el valor de este producto:\n"))
+            # id = int(input("Asigne un id a su nuevo producto:\n"))
+            # nombre = input("Ingrese el nombre del producto:\n")
+            # descripcion = input("Ingrese una descripcion para su producto:\n")
+            # precio = int(input("Ingrese el valor de este producto:\n"))
+                pass
 
         elif op == 'B':
-            pass
+            Api.mostrar_productos()
         elif op == 'C':
-            pass
+
+            id = int(input("Ingrese el id del producto que desea mostrar:\n"))
+            Api.mostrar_producto(id)
         elif op == 'D':
-            pass
+            id = int(input("Ingrese el id del producto que desea modificar:\n"))
+            nombre = input("Ingrese el nuevo nombre o mantenga el anterior:\n")
+            descripcion = input("Ingrese nueva descripción o mantenga la anterior:\n")
+            valor = int(input("Ingrese un nuevo valor o mantenga el anterior:\n"))
+
+            # data = data.append(nombre, descripcion, valor)
+            data = {
+            "id": {id},
+            "nombre": {nombre},
+            "descripcion": {descripcion},
+            "valor": {valor}
+            }
+
+            Api.actualizar_producto(data)
+
         elif op == 'E':
-            pass
+            id = int(input("Ingrese el id del producto que quiere eliminar:\n"))
+            Api.eliminar_producto(id)
         else:
             pass
 
